41fa15cbcabeb2f4e896d8d3374780622ba13e7c
nomac35 <apinar@gmail.com>
2016-04-17 23:07:47 +0000

    new routes added

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,7 @@ Rails.application.routes.draw do
  root 'pages#home'
   #get 'pages/home', to: 'pages#home'
   get 'about', to: 'pages#about'
-  
+  resources :articles
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
 
