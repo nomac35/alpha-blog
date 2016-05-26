a02522acb6b22db839b87d89b80bf65f5b2fb8ab
nomac35 <apinar@gmail.com>
2016-05-26 06:36:06 +0000

    edit and update user function

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -14,6 +14,22 @@ def create
     end
 end
 
+def edit
+    @user = User.find(params[:id])
+
+end
+
+def update
+    @user = User.find(params[:id])
+    if @user.update(user_params)
+        flash[:success] = "Your accout is successfully updated"
+        redirect_to articles_path
+    else
+        render 'edit'
+    end
+    
+
+end
 private
 def user_params
     params.require(:user).permit(:username, :email, :password)
