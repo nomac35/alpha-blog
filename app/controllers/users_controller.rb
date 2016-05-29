d976964cbb5a95476be5dcbf93f148cdc022beb9
nomac35 <apinar@gmail.com>
2016-05-29 18:34:02 +0000

    pagination

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,6 @@
 class UsersController < ApplicationController
 def index
-@users = User.all
+@users = User.paginate(page: params[:page], per_page: 2)
 end
 def new
     @user = User.new
@@ -38,6 +38,7 @@ end
 
 def show
     @user = User.find(params[:id])
+    @user_article = @user.articles.paginate(page: params[:page], per_page: 2)
 end
 
 private
