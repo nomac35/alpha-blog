bbb547d5b89b495b741c49e3c9aea428ef8e0721
nomac35 <apinar@gmail.com>
2016-06-19 07:11:20 +0000

    Categories show and edit added

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -29,7 +29,27 @@ else
 
 end
 
+def edit
+@category = Category.find(params[:id])
+
+end
+
+def update
+@category = Category.find(params[:id])
+if @category.update(category_params)
+    flash[:success] = "Category successfully updated"
+    redirect_to category_path(@category)
+    
+else
+   render 'edit' 
+end
+
+end
+
     def show
+        
+        @category = Category.find(params[:id])
+        @category_articles = @category.articles.paginate(page: params[:page], per_page: 5 )
 
     end
 
